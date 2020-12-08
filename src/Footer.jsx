482e734282218f89381c661f608a1275664f17cf
Shashank-Shastri <shashank.m.shastri@gmail.com>
2020-12-07 23:33:05 -0800

    Added API to fetch an image for the location being searched. Updated logo in header and footer. Changed API call to async/await.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,9 +7,7 @@ const Footer = () => {
 <footer className="text-gray-700 body-font">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
+    <img id="image" className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" src="%PUBLIC_URL%/logo.png" />
      
       <span className="ml-3 text-xl">Altamash Bebal</span>
     </a>
@@ -34,10 +32,7 @@ const Footer = () => {
         </svg>
       </a>
       <a href="https://www.linkedin.com/in/altamash-bebal-171a65181/" target="_blank" className="ml-3 text-gray-500">
-        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
-          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
-        </svg>
+        <img id="image" className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" src="%PUBLIC_URL%/logo.png" />
       </a>
     </span>
   
